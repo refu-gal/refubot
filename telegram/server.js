cd587822879b858bc0ca27b261b3564be7b6f582
Daniel Rodríguez <daniel@dpstudios.es>
2017-03-04 16:52:03 +0100

    Implement kafka topic handle on telegram

--- telegram/server.js
+++ telegram/server.js
@@ -5,6 +5,8 @@ const kafka = require('kafka-node');
 // Bot token
 const TOKEN = process.env.TELEGRAM_TOKEN || '364419216:AAEe1tszpIxOWSLVDNXRs4_3GBUUqsocFCM';
 const KAFKA_ADDRESS = process.env.KAFKA_ADDRESS || 'localhost:2181'
+const KAFKA_OUT_TOPIC = 'telegram_out'
+const KAFKA_IN_TOPIC = 'telegram_in'
 
 // Bot options
 const options = {
@@ -15,19 +17,29 @@ const options = {
 const bot = new TelegramBot(TOKEN, options);
 
 // Initialize kafka
-const Producer = kafka.Producer;
 const client = new kafka.Client(KAFKA_ADDRESS);
-const producer = new Producer(client);
+const producer = new kafka.Producer(client);
 
-// On receive
+// Handle alarm messages
 bot.onText(/alarm (.*)/, (msg, match) => {
   bot.sendMessage(msg.chat.id, 'Thanks I\'ll comunicate that to other people!');
+  chatid = msg.chat.id;
   producer.send([
     {
-      topic: 'telegram_in',
+      topic: KAFKA_IN_TOPIC,
       messages: [ match[1] ],
     }
   ], (err) => {
-    console.log(err);
+    if (err) {
+      bot.sendMessage(msg.chat.id, 'Sorry I\'m having problems to talk with our server');
+    }
   });
+});
+
+// Handle messages coming from kafka "telegram_out" topic
+const consumer = new kafka.Consumer(client, [{
+  topic: KAFKA_OUT_TOPIC,
+}]);
+consumer.on('message', (message) => {
+  bot.sendMessage(chatid, message.value);
 });
