6ae755173e585d8b2ea9036737c33ea3516534e6
antonmry <anton@galiglobal.com>
2017-03-04 18:58:41 +0100

    Fix kafka

--- kafka/kafka.sh
+++ kafka/kafka.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-supervisord -n
+supervisord -n &
 
 sleep 10
 
@@ -10,3 +10,8 @@ sleep 10
 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic facebook_in
 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic facebook_out
 
+for (( ; ; ))
+do
+   echo "Pres CTRL+C to stop..."
+   sleep 100
+done
