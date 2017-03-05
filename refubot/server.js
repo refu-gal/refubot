1a60796d4c5e26e8890cc760ad7ba0e2fbc39ec4
Daniel Rodríguez <daniel@dpstudios.es>
2017-03-05 11:40:43 +0100

    Implementing the logic on refubot

--- refubot/server.js
+++ refubot/server.js
@@ -2,46 +2,95 @@
 const kafka = require('kafka-node');
 
 // Bot token
-const TOKEN = process.env.TELEGRAM_TOKEN || '364419216:AAEe1tszpIxOWSLVDNXRs4_3GBUUqsocFCM';
-//const TOKEN = process.env.TELEGRAM_TOKEN || '133673383:AAGVx28t9c19uqlqOA64Ss7NSEDTwf46YR4';
-
-const KAFKA_OUT_TOPIC = process.env.KAFKA_OUT_TOPIC || 'telegram_out';
-const KAFKA_IN_TOPIC = process.env.KAFKA_IN_TOPIC || 'telegram_in';
-const KAFKA_LIST_TOPIC = process.env.KAFKA_LIST_TOPIC || 'topic_list';
+const KAFKA_ADDRESS = process.env.KAFKA_ADDRESS || 'kafka:2181';
+const KAFKA_LIST_TOPIC = 'topic_list';
+const services = {
+ telegram: {
+   topics: {
+     in: 'telegram_in',
+     out: 'telegram_out',
+   },
+ },
+ facebook: {
+   topics: {
+     in: 'facebook_in',
+     out: 'facebook_out',
+   },
+ },
+};
 
 // Initialize kafka
 const client = new kafka.Client(KAFKA_ADDRESS);
 const producer = new kafka.Producer(client);
 
 producer.on('ready', () => {
-  producer.createTopics([
-    KAFKA_LIST_TOPIC
-  ], (err, data) => {
-    if (err) console.error(err);
-    console.info('Topics created, waiting 5 seconds before start the bot');
-    setTimeout(startBot, 5000);
-  });
+ let topics = [KAFKA_LIST_TOPIC];
+ for (const key in services) {
+   if (!services.hasOwnProperty(key)) continue;
+   const service = services[key];
+   topics.push(service.topics.in);
+ }
+
+ producer.createTopics(topics, (err, data) => {
+   if (err) console.error(err);
+   startBot();
+ });
 });
 
 const startBot = () => {
-  console.info('Starting bot...');
+ console.info('Starting bot...');
 
-  // Handle messages coming from kafka "telegram_out" topic
-  const consumer = new kafka.Consumer(client, [{
-    topic: KAFKA_IN_TOPIC,
-  }]);
-  consumer.on('message', (message) => {
-    const data = JSON.parse(message.value);
-    if (data.id) {
-      bot.sendMessage(data.id, '❗️ ' + data.message);
-    }
-  });
+ // Hanle messages coming from kafka "topic_list" topic
+ const topicsOffset = new kafka.Offset(client);
+ const getTopics = (callback) => {
+   topicsOffset.fetch([
+     {
+       topic: KAFKA_LIST_TOPIC,
+       offset: 0,
+     },
+   ], (err, data) => {
+     console.log(data);
+     if (err) return callback(err, null);
+     const topics = data[KAFKA_LIST_TOPIC];
+     if (callback) callback(null, topics);
+   });
+ };
 
-  // Error handler for the bot
-  const errorHandler = (err) => {
-    if (err) {
-      console.error(err);
-      bot.sendMessage('😞 Sorry I\'ve problems to talk with the server ');
-    }
-  };
+ let topics = [];
+ for (const key in services) {
+   if (!services.hasOwnProperty(key)) continue;
+   const service = services[key];
+   topics.push({topic: service.topics.in});
+ }
+
+ // Handle messages coming from kafka service in topic
+ const consumer = new kafka.Consumer(client, topics);
+ consumer.on('message', (message) => {
+   const data = JSON.parse(message.value);
+
+   if (/register (.*)/.test(data.message)) {
+     const matches = data.message.match(/register (.*)/);
+
+     // Send message to the kafka in topic
+     producer.send([
+       {
+         topic: KAFKA_LIST_TOPIC,
+         messages: [matches[1]],
+       },
+     ], (err) => {
+       if (err) return errorHandler(err);
+       getTopics((err, topics) => {
+         console.log(topics);
+       });
+     });
+   }
+ });
+
+ // Error handler for the bot
+ const errorHandler = (err) => {
+   if (err) {
+     console.error(err);
+     bot.sendMessage('😞 Lo siento pero estoy teninendo problemas para contactar con el servidor.');
+   }
+ };
 };
